40efd0b86c6d0bc0dae8301a152d34d32b6ea362
n1C01451997 <nicolaszarateobando@gmail.com>
2024-02-06 08:51:10 -0500

    ejercicio con el ciclo while

--- bucles.py
+++ bucles.py
@@ -1,1 +1,13 @@
-print ("el costo de la entrada es ")
+import random
+
+secreto = random.randint(1,10)
+
+numero =int(input("adivine el numero entre 1 y 10 "))
+
+while numero != secreto:
+    if numero < secreto :
+        print ("el numero es mayor ")
+    else:
+        print("el numero es menor ")
+    numero = int(input("intenta de nuevo "))
+print("felicidades adivinaste ") 
